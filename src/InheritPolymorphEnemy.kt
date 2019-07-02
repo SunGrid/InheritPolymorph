6be684ac7342170eaefa1b001ce0d9ba5ddf749b
Michael <mlonnecker@gmail.com>
2019-07-02 12:08:08 -0600

    added enum class EnemyClassType and isLight(), isHeavy() and modified open fun attack(enemy: Enemy) with damageToTake -10% or +10%

--- src/InheritPolymorphEnemy.kt
+++ src/InheritPolymorphEnemy.kt
@@ -2,6 +2,15 @@
 
  */
 
+enum class EnemyClassType{
+    LIGHT,
+    HEAVY;
+
+    //fun isLight() : Boolean {return this == LIGHT
+    fun isLight() = this == LIGHT
+    fun isHeavy() = this == HEAVY
+}
+
 interface Healable {
     fun heal(amount: Int)
 }
@@ -25,6 +34,7 @@ abstract class Enemy(health: Int, var weapon: String) {
             }
         }
     var damage: Int = 0
+    var type = EnemyClassType.LIGHT  //Default setting in now LIGHT
 
     init {
         //without this.health = health, passed from mian method, the var health up above will be set to 0 and run at 0
@@ -33,10 +43,38 @@ abstract class Enemy(health: Int, var weapon: String) {
         println("Enemy inint called")
     }
 
+    fun isLight() = type.isLight()
+    fun isHeavy() = type.isHeavy()
+
     //polymorphic parameter
     open fun attack(enemy: Enemy) {
+        val percentage = (damage * 0.1).toInt()  //calculating 10 % of damage
+        var damageToTake = damage
+
+/*
+        if (type == EnemyClassType.LIGHT && enemy.type == EnemyClassType.HEAVY){ //light vs heavy
+            damageToTake = damage - percentage //10% less damage
+        }else if (type == EnemyClassType.HEAVY && enemy.type == EnemyClassType.LIGHT){ // heavy vs light
+            damageToTake = damage + percentage //10% more damage
+        }
+*/
+/*
+
+        if (type.isLight() && enemy.type.isHeavy()){ //light vs heavy
+            damageToTake = damage - percentage //10% less damage
+        }else if (type.isHeavy() && enemy.type.isLight()){ // heavy vs light
+            damageToTake = damage + percentage //10% more damage
+        }
+*/
+        if (isLight() && enemy.isHeavy()){ //light vs heavy
+            damageToTake = damage - percentage //10% less damage
+        }else if (isHeavy() && enemy.isLight()){ // heavy vs light
+            damageToTake = damage + percentage //10% more damage
+        }
+
+
         println("attacking $enemy with $weapon")
-        enemy.takeDamage(damage)
+        enemy.takeDamage(damageToTake)
     }
 
     fun takeDamage(damageToTake: Int) {
@@ -52,6 +90,7 @@ class Pikeman(health: Int, var armor: Int) : Enemy(health, "pike") {
     }
 
     init {
+        type = EnemyClassType.HEAVY
         println("Pikeman init called")
     }
 }
@@ -127,38 +166,60 @@ fun main() {
 
     //vals being declared as "Enemy"
     val pikeman: Enemy = Pikeman(100, 100)
-    pikeman.damage = 5
+    pikeman.damage = 15
     pikeman.run()
 
     val archer: Enemy = Archer(health = 100, arrowCount = 5)
-    archer.damage = 5
+    archer.damage = 10
     archer.run()
 
     val pistolero: Enemy = Pistolero(100,6)
-    pistolero.damage = 10
+    pistolero.damage = 20
     pistolero.run()
 
-    while (archer.health > 0){
-        pistolero.attack(archer)
-        archer.attack(pistolero)
-    }
-    println("archer died")
-    println("pistolero health = ${pistolero.health}")
+    println("pikeman type = ${pikeman.type}")
+    println("archer type = ${archer.type}")
+    println("pistolero type = ${pistolero.type}")
+
+    println("pikeman heavy? ${pikeman.isHeavy()} light? ${pikeman.isLight()}")
+    println("Archer heavy? ${archer.isHeavy()} light? ${archer.isLight()}")
+    println("Pistolero heavy? ${pistolero.isHeavy()} light? ${pistolero.isLight()}")
+
+    pikeman.attack(archer)
+    archer.attack(pikeman)
+    println("pikeman health = ${pikeman.health} Archer Health = ${archer.health}")
+
+    pikeman.attack(pistolero)
+    pistolero.attack(pikeman)
+    println("pikeman health = ${pikeman.health} Pistolero Health = ${pistolero.health}")
+
+    archer.attack(pistolero)
+    pistolero.attack(archer)
+    println("Archer health = ${archer.health} pistolero Health = ${pistolero.health}")
+
+    /*
+       while (archer.health > 0){
+           pistolero.attack(archer)
+           archer.attack(pistolero)
+       }
+
+       println("archer died")
+       println("pistolero health = ${pistolero.health}")
 
 
-       // pistolero.heal(200)//this does not work until in the if statment with smart casting from Kotlin
-    if (pistolero is Healable) {
-        val healable = pistolero as Healable // casting, converting to Healable.
+          // pistolero.heal(200)//this does not work until in the if statment with smart casting from Kotlin
+       if (pistolero is Healable) {
+           val healable = pistolero as Healable // casting, converting to Healable.
 
-        healable.heal(10) //using physical casting
-        println("health = ${pistolero.health}")
+           healable.heal(10) //using physical casting
+           println("health = ${pistolero.health}")
 
-        pistolero.heal(-10) // using smart casting that the if statement created
-        println("health = ${pistolero.health}")
+           pistolero.heal(-10) // using smart casting that the if statement created
+           println("health = ${pistolero.health}")
 
-        pistolero.heal(200)
-        println("health = ${pistolero.health}")
-    }
+           pistolero.heal(200)
+           println("health = ${pistolero.health}")
+       }*/
 
 /*    pikeman.attack(archer)
     println("pikeman health = ${pikeman.health} archer health = ${archer.health}")
